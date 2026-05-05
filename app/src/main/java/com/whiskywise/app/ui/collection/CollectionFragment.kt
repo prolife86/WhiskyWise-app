8d97a507992737553c40cbbee2bec1d664fe85d0
Heisenbugger <49482721+prolife86@users.noreply.github.com>
2026-05-05 10:20:12 +0200

    Update CollectionFragment.kt

--- app/src/main/java/com/whiskywise/app/ui/collection/CollectionFragment.kt
+++ app/src/main/java/com/whiskywise/app/ui/collection/CollectionFragment.kt
@@ -7,6 +7,7 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.chip.Chip
 import com.whiskywise.app.R
 import com.whiskywise.app.databinding.FragmentCollectionBinding
@@ -30,6 +31,7 @@ class CollectionFragment : Fragment() {
                 bundleOf("whiskyId" to whisky.id),
             )
         }
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.adapter = adapter
         binding.swipeRefresh.setOnRefreshListener { vm.load() }
 
